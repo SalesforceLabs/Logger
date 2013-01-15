23bb788afdaa51ae72ee6e633579115415f00239
Sönke Rohde <soenke.rohde@gmail.com>
2013-01-15 13:47:02 -0300

    upgrade iOS sdk to 1.4.5

--- xcode/Sales Logger/SalesforceHybridSDK.framework/Versions/A/Headers/CDVPlugin.h
+++ xcode/Sales Logger/SalesforceHybridSDK.framework/Versions/A/Headers/CDVPlugin.h
@@ -6,9 +6,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -23,28 +23,27 @@
 #import "NSMutableArray+QueueAdditions.h"
 #import "CDVCommandDelegate.h"
 
-#define CDVPluginHandleOpenURLNotification	@"CDVPluginHandleOpenURLNotification"
+#define CDVPluginHandleOpenURLNotification @"CDVPluginHandleOpenURLNotification"
+#define CDVPluginResetNotification @"CDVPluginResetNotification"
 
-#define VERIFY_ARGUMENTS(args, expectedCount, callbackId) if (![self verifyArguments:args withExpectedCount:expectedCount andCallbackId:callbackId \
-callerFileName:__FILE__ callerFunctionName:__PRETTY_FUNCTION__]) { return; }
+@interface CDVPlugin : NSObject {}
 
-
-@interface CDVPlugin : NSObject {
-}
-
-@property (nonatomic, retain) UIWebView* webView;
-@property (nonatomic, retain) NSDictionary* settings;
-@property (nonatomic, retain) UIViewController* viewController;
-@property (nonatomic, retain) id<CDVCommandDelegate> commandDelegate;
+// TODO(agrieve): Make these zeroing weak refs once we drop support for 4.3.
+@property (nonatomic, unsafe_unretained) UIWebView* webView;
+@property (nonatomic, strong) NSDictionary* settings;
+@property (nonatomic, unsafe_unretained) UIViewController* viewController;
+@property (nonatomic, unsafe_unretained) id <CDVCommandDelegate> commandDelegate;
 
 @property (readonly, assign) BOOL hasPendingOperation;
 
-- (CDVPlugin*) initWithWebView:(UIWebView*)theWebView settings:(NSDictionary*)classSettings;
-- (CDVPlugin*) initWithWebView:(UIWebView*)theWebView;
+- (CDVPlugin*)initWithWebView:(UIWebView*)theWebView settings:(NSDictionary*)classSettings;
+- (CDVPlugin*)initWithWebView:(UIWebView*)theWebView;
 
-- (void) handleOpenURL:(NSNotification*)notification;
-- (void) onAppTerminate;
-- (void) onMemoryWarning;
+- (void)handleOpenURL:(NSNotification*)notification;
+- (void)onAppTerminate;
+- (void)onMemoryWarning;
+- (void)onReset;
+- (void)dispose;
 
 /*
  // see initWithWebView implementation
@@ -54,25 +53,11 @@ callerFileName:__FILE__ callerFunctionName:__PRETTY_FUNCTION__]) { return; }
  - (void) onOrientationDidChange {}
  */
 
-- (id) appDelegate;
-- (UIViewController*) appViewController 
-#ifdef __clang__
-__attribute__ ((deprecated("Use the viewController property instead. This will be removed in Cordova 2.0.0")));
-#else
-__attribute__ ((deprecated()));
-#endif
+- (id)appDelegate;
 
-- (NSString*) writeJavascript:(NSString*)javascript;
-- (NSString*) success:(CDVPluginResult*)pluginResult callbackId:(NSString*)callbackId;
-- (NSString*) error:(CDVPluginResult*)pluginResult callbackId:(NSString*)callbackId;
-
-
-- (BOOL) verifyArguments:(NSMutableArray*)arguments withExpectedCount:(NSUInteger)expectedCount andCallbackId:(NSString*)callbackId 
-		  callerFileName:(const char*)callerFileName callerFunctionName:(const char*)callerFunctionName
-#ifdef __clang__
-__attribute__ ((deprecated("This will be removed in Cordova 2.0.0")));
-#else
-__attribute__ ((deprecated()));
-#endif
+// TODO(agrieve): Deprecate these in favour of using CDVCommandDelegate directly.
+- (NSString*)writeJavascript:(NSString*)javascript;
+- (NSString*)success:(CDVPluginResult*)pluginResult callbackId:(NSString*)callbackId;
+- (NSString*)error:(CDVPluginResult*)pluginResult callbackId:(NSString*)callbackId;
 
 @end
